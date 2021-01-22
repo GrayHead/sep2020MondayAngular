f0f764319f1874f464636bcdfe8bd8e4ca23d598
GrayHead <ss.zhuravlov@gmail.com>
2021-01-22 22:05:01 +0200

    route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {users} from '../data/users';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +6,6 @@ import {users} from '../data/users';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  users = users;
 
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,26 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import { UserComponent } from './components/user/user.component';
+import {UserComponent} from './components/user/user.component';
+import {RouterModule} from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {UsersComponent} from './components/users/users.component';
+import {FullUserComponent} from './components/full-user/full-user.component';
 
 @NgModule({
-  declarations: [AppComponent, UserComponent],
+  declarations: [AppComponent, UserComponent, HomeComponent, UsersComponent, FullUserComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'link/home', component: HomeComponent
+      },
+      {
+        path: 'link/users', component: UsersComponent, children: [
+          {path: ':id', component: FullUserComponent}
+        ]
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
