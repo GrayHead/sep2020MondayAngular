6be9bb6614d4e4f0841e67a8c5a428f17eac02e9
GrayHead <ss.zhuravlov@gmail.com>
2021-01-22 21:16:02 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    UsersComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
