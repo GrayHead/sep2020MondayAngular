a113c0ca82cf02d04533477127e57721d07d7e3a
GrayHead <ss.zhuravlov@gmail.com>
2021-01-22 21:32:36 +0200

    if classes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
-import { UsersComponent } from './users/users.component';
-import { UserComponent } from './user/user.component';
+import {AppComponent} from './app.component';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MenuComponent,
-    UsersComponent,
-    UserComponent
-  ],
+  declarations: [AppComponent, UserComponent],
   imports: [
     BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
